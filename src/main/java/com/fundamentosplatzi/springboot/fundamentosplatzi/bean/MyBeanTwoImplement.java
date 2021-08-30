54ac24028ff9fdfbb9da27ceff77e471ac275e8d
leotalorac <leotalorac@unal.edu.co>
2021-08-30 10:51:06 -0500

    add a new  bean implementation and configuration changes to return

---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentosplatzi/bean/MyBeanTwoImplement.java
@@ -0,0 +1,8 @@
+package com.fundamentosplatzi.springboot.fundamentosplatzi.bean;
+
+public class MyBeanTwoImplement implements MyBean{
+    @Override
+    public void print() {
+        System.out.println("Hello from my second Bean implementation :3");
+    }
+}
--- src/main/java/com/fundamentosplatzi/springboot/fundamentosplatzi/configuration/MyConfigurationBean.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentosplatzi/configuration/MyConfigurationBean.java
@@ -2,6 +2,7 @@ package com.fundamentosplatzi.springboot.fundamentosplatzi.configuration;
 
 import com.fundamentosplatzi.springboot.fundamentosplatzi.bean.MyBean;
 import com.fundamentosplatzi.springboot.fundamentosplatzi.bean.MyBeanImplement;
+import com.fundamentosplatzi.springboot.fundamentosplatzi.bean.MyBeanTwoImplement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 public class MyConfigurationBean {
     @Bean
     public MyBean beanOperation(){
-        return new MyBeanImplement();
+//        change the correct implementation
+        return new MyBeanTwoImplement();
     }
 }

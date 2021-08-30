b66ea5eecf4e3ec57a3dac3351049e7f6868ea82
leotalorac <leotalorac@unal.edu.co>
2021-08-30 10:33:15 -0500

    initial commit

---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentosplatzi/FundamentosplatziApplication.java
@@ -0,0 +1,14 @@
+package com.fundamentosplatzi.springboot.fundamentosplatzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosplatziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosplatziApplication.class, args);
+	}
+
+}
+	

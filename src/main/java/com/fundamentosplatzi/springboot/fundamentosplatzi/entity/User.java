8f28a699624501226a769cb8e1e4b9c5f85aa5cc
leotalorac <leotalorac@unal.edu.co>
2021-08-31 08:51:54 -0500

    JPA entities

---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentosplatzi/entity/User.java
@@ -0,0 +1,87 @@
+package com.fundamentosplatzi.springboot.fundamentosplatzi.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "user")
+public class User {
+    //    table structure and attributes
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id_user",nullable = false,unique = true)
+    private Long id;
+    @Column(length = 50)
+    private String name;
+    @Column(length = 50)
+    private String email;
+
+    private LocalDate birthDate;
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    @JsonManagedReference
+    private List<Post> posts = new ArrayList<>();
+
+    public User() {
+    }
+
+    public User(String name, String email, LocalDate birthDate) {
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", birthDate=" + birthDate +
+                ", posts=" + posts +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+}
